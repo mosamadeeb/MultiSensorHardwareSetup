cc9f9e1e5686ebe2c3de3b6c966744f1584a4640
SutandoTsukai181 <52977072+SutandoTsukai181@users.noreply.github.com>
2024-05-03 20:20:24 +0300

    Remove unused code

--- src/main.cpp
+++ src/main.cpp
@@ -146,17 +146,9 @@ QMC5883LCompass hmc;
 
 //#define OUTPUT_READABLE_MAGNET
 
-// uncomment "OUTPUT_TEAPOT" if you want output that matches the
-// format used for the InvenSense teapot demo
-//#define OUTPUT_TEAPOT
-
-#define INTERRUPT_PIN 2  // use pin 2 on Arduino Uno & most boards
-#define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
 bool blinkState = false;
-const int reedSwitchPin = 3;
 // MPU control/status vars
 bool dmpReady = false;  // set true if DMP init was successful
-uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
 uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
 uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
 uint16_t fifoCount;     // count of all bytes currently in FIFO
@@ -172,25 +164,12 @@ VectorInt16 gyro;
 float euler[3];         // [psi, theta, phi]    Euler angle container
 float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
-// packet structure for InvenSense teapot demo
-uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
-
 void inline SerialPrintTitle(const char* text) {
 #ifdef SERIAL_PRINT_TITLE
     Serial.print(text);
     Serial.print("\t");
 #endif
 }
-
-// ================================================================
-// ===               INTERRUPT DETECTION ROUTINE                ===
-// ================================================================
-
-volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
-void dmpDataReady() {
-    mpuInterrupt = true;
-}
-
 
 
 // ================================================================
@@ -206,14 +185,11 @@ void setup() {
     Fastwire::setup(400, true);
 #endif
 
-//    debug_init();
-
     // initialize serial communication
     // (115200 chosen because it is required for Teapot Demo output, but it's
     // really up to you depending on your project)
     Serial.begin(115200);
     while (!Serial); // wait for Leonardo enumeration, others continue immediately
-    pinMode(reedSwitchPin, INPUT);
     // NOTE: 8MHz or slower host processors, like the Teensy @ 3.3V or Arduino
     // Pro Mini running at 3.3V, cannot handle this baud rate reliably due to
     // the baud timing being too misaligned with processor ticks. You must use
@@ -223,7 +199,6 @@ void setup() {
     // initialize device
 //    Serial.println(F("Initializing I2C devices..."));
     mpu.initialize();
-    pinMode(INTERRUPT_PIN, INPUT);
 
     hmc.init();
 
@@ -276,13 +251,6 @@ void setup() {
 //        Serial.println(F("Enabling DMP..."));
         mpu.setDMPEnabled(true);
 
-        // enable Arduino interrupt detection
-//        Serial.print(F("Enabling interrupt detection (Arduino external interrupt "));
-//        Serial.print(digitalPinToInterrupt(INTERRUPT_PIN));
-//        Serial.println(F(")..."));
-        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
-        mpuIntStatus = mpu.getIntStatus();
-
         // set our DMP Ready flag so the main loop() function knows it's okay to use it
 //        Serial.println(F("DMP ready! Waiting for first interrupt..."));
         dmpReady = true;
@@ -299,11 +267,13 @@ void setup() {
 //        Serial.println(F(")"));
     }
 
+#ifdef LED_PIN
     // configure LED for output
     pinMode(LED_PIN, OUTPUT);
 
     // Turn on LED to indicate that filter is not working
     digitalWrite(LED_PIN, true);
+#endif
 }
 
 
@@ -313,15 +283,6 @@ void setup() {
 // ================================================================
 
 void loop() {
-    int reedSwitchState = digitalRead(reedSwitchPin);
-//    if (reedSwitchState == HIGH) {
-//        //Serial.println("Reed Switch state: LOW (No magnetic field detected)");
-//        Serial.print("0,");
-//    } else {
-//        //Serial.println("Reed Switch state: HIGH (Magnetic field detected)");
-//        Serial.print("1,");
-//    }
-
     // if programming failed, don't try to do anything
     if (!dmpReady) return;
 
@@ -423,8 +384,10 @@ void loop() {
 
                 kalmanSet = true;
 
+#ifdef LED_PIN
                 // Turn off LED to indicate that filter is working
                 digitalWrite(LED_PIN, false);
+#endif
             }
 
             kalmanPredictions[0] = aaWorld.x;
@@ -478,24 +441,5 @@ void loop() {
         Serial.print("\t");
         Serial.println(hmc.getZ());
 #endif
-
-#ifdef OUTPUT_TEAPOT
-        // display quaternion values in InvenSense Teapot demo format:
-            teapotPacket[2] = fifoBuffer[0];
-            teapotPacket[3] = fifoBuffer[1];
-            teapotPacket[4] = fifoBuffer[4];
-            teapotPacket[5] = fifoBuffer[5];
-            teapotPacket[6] = fifoBuffer[8];
-            teapotPacket[7] = fifoBuffer[9];
-            teapotPacket[8] = fifoBuffer[12];
-            teapotPacket[9] = fifoBuffer[13];
-            Serial.write(teapotPacket, 14);
-            teapotPacket[11]++; // packetCount, loops at 0xFF on purpose
-#endif
-//        Serial.println("_____________________________________________________________");
-//        delay(300);
-        // blink LED to indicate activity
-//        blinkState = !blinkState;
-//        digitalWrite(LED_PIN, blinkState);
     }
 }
